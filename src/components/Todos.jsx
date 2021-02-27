166e4977958d6e68e664ede9e3ba9d3036fdecbd
Squidward_Tenpoles <65642638+KadalGoreng@users.noreply.github.com>
2021-02-28 01:51:49 +0700

    gawe pages karo komponen todo

---
+++ src/components/Todos.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import Todo from "./Todo";
+
+const Todos = () => {
+  const todos = [
+    {
+      text: "Belajar 1"
+    },
+    {
+      text: "Belajar 2"
+    },
+    {
+      text: "Belajar 3"
+    },
+    {
+      text: "Belajar 4"
+    },
+    {
+      text: "Belajar 5"
+    },
+    {
+      text: "Belajar 6"
+    },
+    {
+      text: "Belajar 7"
+    },
+    {
+      text: "Belajar 8"
+    },
+    {
+      text: "Belajar 9"
+    },
+    {
+      text: "Belajar 10"
+    }
+  ];
+
+  return (
+    <section className="todos">
+      {todos.map((todo) => {
+        return <Todo text={todo.text} />;
+      })}
+    </section>
+  );
+};
+
+export default Todos;
